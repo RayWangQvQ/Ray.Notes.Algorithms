fd1aad71a4bad52e5844547bea7bb328f74d92e6
RayWang <2271272653@qq.com>
2021-10-15 18:17:36 +0800

    Update

--- Ray.EssayNotes.Algorithms/027.移除元素/UnitTest027.cs
+++ Ray.EssayNotes.Algorithms/027.移除元素/UnitTest027.cs
@@ -18,19 +18,53 @@ namespace Ray.EssayNotes.Algorithms
 
         public class Solution
         {
+            /* 用双指针法，
+             思路是，最后数组左边是排出后的有用的数字
+             第一个指针用来指向有用数字的最后一个下表，第二个指针用来向右依次遍历数组，遇到val就跳过，遇到有用的，则将其赋值到idx
+             */
+
             public int RemoveElement(int[] nums, int val)
             {
-                int n = nums.Length;
-                int left = 0;
-                for (int right = 0; right < n; right++)
+                int idx = 0;
+                for (int search = 0; search < nums.Length; search++)
                 {
-                    if (nums[right] != val)
+                    //如果是val，就跳过，继续向右搜索
+                    if (nums[search] == val) continue;
+
+                    //如果不是val，就将它赋值到idx位置
+                    nums[idx] = nums[search];
+                    idx++;
+                }
+                return idx;
+            }
+        }
+
+        public class Solution2
+        {
+            /*
+             * 双指针优化（首尾指针法）
+             *
+             */
+
+            public int RemoveElement(int[] nums, int val)
+            {
+                int idx = 0;
+                int search = nums.Length;
+                while (idx != search)
+                {
+                    if (nums[idx] == val)
                     {
-                        nums[left] = nums[right];
-                        left++;
+                        nums[idx] = nums[search];
+                        search--;
+                    }
+                    else
+                    {
+                        idx++;
                     }
                 }
-                return left;
+
+                if (nums[idx] == val) return idx;
+                else return idx++;
             }
         }
     }
